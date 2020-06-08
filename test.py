2dfe6d1654b0d0efcb6bb09682765b1d66c944f0
Irwan Syarifudin <irwan.syarifudin@sleekr.co>
2020-06-08 09:37:09 +0700

    first webium

--- test.py
+++ test.py
@@ -7,12 +7,16 @@ from webium import BasePage, Find, Finds
 class MekariPage(BasePage):
 	url = 'https://mekari.com'
 
-	buttonPelajariMekari = Find(by=By.XPATH, value="//a[contains(text(),'Pelajari Tentang Mekari')]")
+	button = Find(by=By.XPATH, value="//a[contains(text(),'Pelajari Tentang Mekari')]")
 
-class AssertPage()
+class AssertPage(WebElement):
+	page_heading = Find(by=By.CLASS_NAME, value="h1.u-str-48")
+
 if __name__ == '__main__':
 	home_page = MekariPage()
 	home_page.open()
 	home_page.buttonPelajariMekari.click()
+	result_page = AssertPage()
+	print('This should be True: ' + str(result_page.is_element_present('page_heading')))
 	print('Tested')
 	get_driver().quit()
